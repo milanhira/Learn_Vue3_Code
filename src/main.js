639bcd178f939375157a191399173f77971dac90
Milan Hira <48389651+milanhira@users.noreply.github.com>
2020-12-29 12:35:49 +0600

    add VueBootstrap Registration and Run npm install vue bootstrap-vue bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//Add VueBootstrap vue Registration
+
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+// EndAdd VueBootstrap vue Registration
+
 createApp(App).mount('#app')
